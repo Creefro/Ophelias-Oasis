8ae1cec197324faead9699b110122e7846720edd
guney <guneykaya2002@gmail.com>
2022-05-04 19:15:17 +0300

    Add project files.

---
+++ OPHELIA’S OASIS/Müşteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPHELIA_S_OASIS
+{
+    class Müşteri
+    {
+        public string MüşteriAd { get; set; }
+        public string MüşteriSoyad { get; set; }
+        public string Rezervasyon { get; set; }
+        public string Status { get; set; }
+        public string Eposta { get; set; }
+        public int KrediKartıNo { get; set; }
+    }
+}
---
+++ OPHELIA’S OASIS/Çalışan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPHELIA_S_OASIS
+{
+    class Çalışan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string KullanıcıAdı { get; set; }
+        public string Şifre { get; set; }
+    }
+}
